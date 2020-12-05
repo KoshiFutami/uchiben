31dda4c9689967a09f2c41dbf6073e99f40c7440
Koshi Futami <koshi.futami@gmail.com>
2020-12-05 13:31:19 +0900

    Add comments list in vocabs#index

--- app/javascript/common/index.js
+++ app/javascript/common/index.js
@@ -6,4 +6,15 @@ $(function(){
     $(btnOpenNav).on('click', function(){
         $(sideNav).addClass('is-active');
     });
+
+});
+
+$(function(){
+    const commentArea = $('.vocab__comments');
+    const openCommentBtn = $('.vocab__btn--comment');
+    openCommentBtn.on("click", function(){
+        $(this).parent().next(commentArea).toggleClass("is-active");
+        $(this).prop('title', 'コメントを閉じる');
+    });
+    
 });

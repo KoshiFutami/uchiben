1756c1ea85ea4de3efb7f2454d3c73270b99673f
Koshi Futami <koshi.futami@gmail.com>
2020-12-16 09:28:46 +0900

    Add comments#create with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,19 @@ class CommentsController < ApplicationController
     @comment = @vocab.comments.new(comment_params)
     @comment.user_id = current_user.id
 
-    if @comment.save
-      # redirect_to user_vocab_path(@vocab.user, @vocab)
-      flash[:notice] = 'コメントが投稿されました。'
-      redirect_back(fallback_location: root_path)
-    else
-      # redirect_to user_vocab_path(@vocab.user, @vocab)
-      redirect_back(fallback_location: root_path) 
-    end
+    # if @comment.save
+    #   # redirect_to user_vocab_path(@vocab.user, @vocab)
+    #   flash[:notice] = 'コメントが投稿されました。'
+    #   redirect_back(fallback_location: root_path)
+    # else
+    #   # redirect_to user_vocab_path(@vocab.user, @vocab)
+    #   redirect_back(fallback_location: root_path) 
+    # end
+
+    # ajaxで書き換え
+    @comment.save!
+
+
   end
 
   def destroy
